5f5310a968357408be19c8ea8b6d7b6fccd17c14
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-07-02 21:19:33 -0700

    starting with polars in rust

--- practicing/src/main.rs
+++ practicing/src/main.rs
@@ -1,18 +1,5 @@
-
+pub mod polars;
+use crate::polars::using_polars;
 fn main() {
-    
-    // CLOSURES
-
-    // Can a closure be a function using impl?
-    pub struct LivingBeing {
-        cells:&'static str,
-        cellular_org:String,
-        specie:String
-    }
-
-    let _human = LivingBeing{
-        cells:"Eukariote",
-        cellular_org: String::from("Pluricellular"),
-        specie:String::from("Homo sapiens")
-    };
+    using_polars()
 }
---
+++ practicing/src/polars.rs
@@ -0,0 +1,6 @@
+use polars::prelude::*;
+
+pub fn using_polars() {
+    println!("Creamos un archivo aparte con una funcion y se declara como un modulo en la funcion main");
+        
+}
