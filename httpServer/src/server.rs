94ce48d38c1753e4eb8507bf139194e52dfd6d90
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-11-25 12:05:19 -0800

    using match to handle socket connection and request receiving  errors

--- httpServer/src/server.rs
+++ httpServer/src/server.rs
@@ -1,12 +1,32 @@
+use std::net::TcpListener;
+use std::io::Read;
+
 pub struct Server {
-        addr: String,
+    addr: String,
+}
+impl Server {
+    pub fn new(addr: String) -> Self {
+        return Self { addr: addr };
     }
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            return Self { addr: addr };
-        }
-        pub fn run(self) {
-            // Self takes ownership
-            println!("Listening on {}", self.addr);
+    pub fn run(self) {
+        // Self takes ownership
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream,_)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                        }
+                    }
+                },
+                Err(i) =>{println!("Failed to stablish a connection: {}", i);}
+            }
         }
     }
+}
--- httpServer/target/debug/deps/http_server-16f67a5acc625086.d
+++ httpServer/target/debug/deps/http_server-16f67a5acc625086.d
@@ -1,6 +1,9 @@
-/Users/jjpc/Documents/learning_Rust/httpServer/target/debug/deps/http_server-16f67a5acc625086: src/main.rs src/server.rs
+/Users/jjpc/Documents/learning_Rust/httpServer/target/debug/deps/http_server-16f67a5acc625086: src/main.rs src/server.rs src/http/mod.rs src/http/request.rs src/http/method.rs
 
-/Users/jjpc/Documents/learning_Rust/httpServer/target/debug/deps/http_server-16f67a5acc625086.d: src/main.rs src/server.rs
+/Users/jjpc/Documents/learning_Rust/httpServer/target/debug/deps/http_server-16f67a5acc625086.d: src/main.rs src/server.rs src/http/mod.rs src/http/request.rs src/http/method.rs
 
 src/main.rs:
 src/server.rs:
+src/http/mod.rs:
+src/http/request.rs:
+src/http/method.rs:
