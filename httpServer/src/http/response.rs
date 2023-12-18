0ffb1a2e24d24060e3b6b532e96cb848ce95f2d5
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-12-18 12:48:25 -0500

    moving to dynamic and static dispatch

--- httpServer/src/http/response.rs
+++ httpServer/src/http/response.rs
@@ -1,6 +1,6 @@
-use std::fmt::{Display, Formatter, Result as FmtResult};
-
+use std::{fmt::{Display, Formatter, Result as FmtResult}, net::TcpStream};
 use super::status_code::StatusCode;
+use std::io::{Write, Result as IoResult};
 
 pub struct Response {
     status_code: StatusCode,
@@ -12,16 +12,13 @@ impl Response{
     pub fn new (status_code:StatusCode, body:Option<String>) -> Self {
         Response{status_code, body}
     }
-}
-
-impl Display for Response{
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    pub fn send (&self, stream:&mut dyn Write) -> IoResult<()> {
         let body = match &self.body {
             Some(b) => b,
             None => ""
         };
 
-        write!(f, "HTTP/1.1 {}  {} \r\n\r\n{} ",
+        write!(stream, "HTTP/1.1 {}  {} \r\n\r\n{} ",
         self.status_code,
         self.status_code.reason_phrase(),
         body
